84e06d331de0521549d8c1815f16a230e426ed8c
Andrew Trent <andrewtrentdesign@gmail.com>
2018-10-24 23:36:26 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-	programmer_hash[0] << :yukihiro_matsumoto
+	programmer_hash[0] = :yukihiro_matsumoto
 	return programmer_hash
 
 end
@@ -125,5 +125,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	programmer_hash[:dennis_ritchie][:languages][0] = "Assembly"
 end
